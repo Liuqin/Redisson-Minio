96b0e11c1fdce59e1740673ada6ef9856f23c630
Liuqin <sata123456!>
2020-06-20 21:12:58 +0800

    123

--- base/src/main/java/org/qin/base/minio/MinioOperator.java
+++ base/src/main/java/org/qin/base/minio/MinioOperator.java
@@ -18,7 +18,6 @@ import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.concurrent.*;
 
 /**
  * @title: MinioTemplate
